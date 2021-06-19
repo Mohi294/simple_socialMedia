07138f735a41d0f92bb0cc3d4212d4b86fb5a78d
mohi294 <mahmadkhani294@gmail.com>
2021-06-19 06:53:38 +0430

    test done for user login and registeration

--- social_media/user/views.py
+++ social_media/user/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model, authenticate
-from user.serializers import UserSerializer, ChangePasswordSerializer, UpdateUserSerializer
+from user.serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.views.generic.edit import UpdateView
 from rest_framework import generics
