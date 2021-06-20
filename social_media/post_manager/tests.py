24617dc5d30acca553831de32ca5f1a409e3059f
mohi294 <mahmadkhani294@gmail.com>
2021-06-20 06:25:43 +0430

    tests with django test generator

--- social_media/post_manager/tests.py
+++ social_media/post_manager/tests.py
@@ -1,10 +1,12 @@
 import json
 import string
 from random import randint
-import factory
 
+import factory
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.forms.models import model_to_dict
 from django.test import TestCase
 from django.urls import reverse
 from factory import Iterator, LazyAttribute, SubFactory, lazy_attribute
@@ -48,3 +50,77 @@ class CommentFactory(DjangoModelFactory):
     post = SubFactory(PostFactory)
     comment = LazyAttribute(lambda x: faker.paragraph(
         nb_sentences=3, variable_nb_sentences=True))
+
+
+class TestCasePost(TestCase):
+
+    def test_create(self):
+        """
+        Test the creation of a Post model using a factory
+        """
+        post = PostFactory.create()
+        self.assertEqual(Post.objects.count(), 1)
+
+    def test_create_batch(self):
+        """
+        Test the creation of 5 Post models using a factory
+        """
+        posts = PostFactory.create_batch(5)
+        self.assertEqual(Post.objects.count(), 5)
+        self.assertEqual(len(posts), 5)
+
+    def test_attribute_count(self):
+        """
+        Test that all attributes of Post server are counted. It will count the primary key and all editable attributes.
+        This test should break if a new attribute is added.
+        """
+        post = PostFactory.create()
+        post_dict = model_to_dict(post)
+        self.assertEqual(len(post_dict.keys()), 4)
+
+    def test_attribute_content(self):
+        """
+        Test that all attributes of Post server have content. This test will break if an attributes name is changed.
+        """
+        post = PostFactory.create()
+        self.assertIsNotNone(post.id)
+        self.assertIsNotNone(post.owner)
+        self.assertIsNotNone(post.created_at)
+        self.assertIsNotNone(post.text)
+
+
+class TestCaseComment(TestCase):
+
+    def test_create(self):
+        """
+        Test the creation of a Comment model using a factory
+        """
+        comment = CommentFactory.create()
+        self.assertEqual(Comment.objects.count(), 1)
+
+    def test_create_batch(self):
+        """
+        Test the creation of 5 Comment models using a factory
+        """
+        comments = CommentFactory.create_batch(5)
+        self.assertEqual(Comment.objects.count(), 5)
+        self.assertEqual(len(comments), 5)
+
+    def test_attribute_count(self):
+        """
+        Test that all attributes of Comment server are counted. It will count the primary key and all editable attributes.
+        This test should break if a new attribute is added.
+        """
+        comment = CommentFactory.create()
+        comment_dict = model_to_dict(comment)
+        self.assertEqual(len(comment_dict.keys()), 4)
+
+    def test_attribute_content(self):
+        """
+        Test that all attributes of Comment server have content. This test will break if an attributes name is changed.
+        """
+        comment = CommentFactory.create()
+        self.assertIsNotNone(comment.id)
+        self.assertIsNotNone(comment.comment_by)
+        self.assertIsNotNone(comment.post)
+        self.assertIsNotNone(comment.comment)
--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,6 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'social_media.urls'
+
+
 
 TEMPLATES = [
     {
